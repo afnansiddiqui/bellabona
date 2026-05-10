6762edf29b4edffee10da5ea2513100447d7d689
afnansiddiqui <afnan.sid0@gmail.com>
2026-05-11 04:00:34 +0500

    updated

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -6,9 +6,14 @@
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../../sanity.config";
+
+export const dynamic = "force-dynamic";
+
+export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
